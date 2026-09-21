aaa8bb34bc12ad23e5bf2d24cfc83bc51ccee6bb
ssypchenko <30598453+ssypchenko@users.noreply.github.com>
2026-09-21 01:03:42 +0100

    Bump plugin version to 0.7.27

--- GunGameBotAI.cs
+++ GunGameBotAI.cs
@@ -71,7 +71,7 @@ public sealed class GunGameBotAI : BasePlugin, IPluginConfig<GunGameBotAIConfig>
     }
 
     public override string ModuleName => "GunGame Bot AI";
-    public override string ModuleVersion => "0.7.26";
+    public override string ModuleVersion => "0.7.27";
     public override string ModuleAuthor => "Sergey";
     public override string ModuleDescription => "Bounded GunGame bot behaviour improvements.";
 
